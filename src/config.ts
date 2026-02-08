c5aa773198f5572a00a3cc39d411d03711eb33f2
XueGao <zihang518@outlook.com>
2026-02-08 13:46:05 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -133,7 +133,7 @@ export const imageFallbackConfig: ImageFallbackConfig = {
 
 export const umamiConfig: UmamiConfig = {
 	enable: true,
-	baseUrl: "https://cloud.umami.is/",
+	baseUrl: "https://cloud.umami.is",
 	shareId: "fc59d705-e252-4296-88c0-e37ef66e97c5",
 	timezone: "Asia/Shanghai",
 };
